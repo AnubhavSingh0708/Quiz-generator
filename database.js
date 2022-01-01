b22c167610321fdcd39a6d08de05f87dfe845357
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2022-01-01 13:24:29 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,44 @@
+const database=[{
+"topics":[{
+"science":[{
+"What of these is smallest unit of matter that exists freely":[
+"Molecule",
+"Atom",
+"Cell",
+"Quark"
+]
+}],
+"biology":[{
+"Full form of RNA":[
+"Ribonucleic acid",
+"Ribonuclear acid",
+"Ribbonnatural alcali",
+"Rodonuclear acid"
+]
+}],
+"chemistry":[{
+"Chemical with smell like rotten eggs":[
+"Hydrogen sulfide",
+"hydous oxide",
+"nitrous oxide",
+"buckministerfullerene"
+]
+}],
+"physics":[{
+"Acceeration due to gravity on earth":[
+"3*10^8 meter/s",
+"9.81 meter/s",
+"330 meter/s",
+"10 meter/s"
+]
+}],
+"astronomy":[{
+"Where is sea of tranquility located":[
+"Moon",
+"Andromeda galaxy",
+"Pacific ocean",
+"Jupiter"
+]
+}]
+}]
+}];
---
+++ script.js
@@ -0,0 +1,16 @@
+var qb= database;
+var topics=qb[0].topics;
+var ltop;
+ltop=Object.getOwnPropertyNames((topics[0]));
+function findTex(){
+document.getElementById("result").innerHTML="";
+var input=document.getElementById("search").value;
+input=input.toLowerCase();
+for (let i = 0; i < ltop.length; i++) {
+var pos=ltop[i].search(input);
+if (pos<0){
+} else {
+document.getElementById("result").innerHTML+=ltop[i]+"<bR>";
+}
+}
+}
